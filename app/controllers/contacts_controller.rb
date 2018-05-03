f546f57ea6baafa1c0c7f07e6868972ca28efc52
Simon Jordan <simon_jordan@live.com>
2018-05-03 17:05:33 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,13 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
+      #if customer succesfully signs up, he will receive an email using the below code.
+        name = params[:contact][:name]
+        email = params[:contact][:email]
+        body = params[:contact][:comments]
+      
+      ContactMailer.contact_email(name, email, body).deliver
+      #customer will receive notification using flash if succesfully or not.
       flash[:success] = 'Message Sent'
       redirect_to new_contact_path
     else
