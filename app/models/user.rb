31734c03017a1cd8351a2c10d3695fced6b36dc3
Simon Jordan <simon_jordan@live.com>
2018-05-18 22:26:54 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
   attr_accessor :stripe_card_token
+  has_one :profile
   
   def save_with_payment
     if valid?
