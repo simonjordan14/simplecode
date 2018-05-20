3d52da30f5ce61335543849706544263a6899f89
Simon Jordan <simon_jordan@live.com>
2018-05-20 23:16:19 +0000

    Added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,9 @@
 class ProfilesController < ApplicationController
+    def new
+        #from where a user can fill out their own profile.
+                            #below array identitifes whos logged in.
+        @user = User.find(:params[user_id])
+        #prepare form for profile
+        @profile = @user.build_profile
+    end
 end
